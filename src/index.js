470951d403fda72f6d8fc638902844131397c3e5
elo9001535 <diana.pontes@elo.com.br>
2022-05-20 13:42:28 -0300

    export checkbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./components/ts-button";
 import "./components/ts-button-group";
 import "./components/ts-button-icon";
+import "./components/ts-checkbox";
 import "./components/ts-icon";
 import "./components/ts-link";
