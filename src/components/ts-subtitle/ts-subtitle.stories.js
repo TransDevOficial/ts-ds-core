b1b1dcef3dd2d6fe3eab08c23114a9469db0d841
elo9001535 <diana.pontes@elo.com.br>
2022-05-27 19:29:35 -0300

    create docs for ts-subtitle

--- src/components/ts-subtitle/ts-subtitle.stories.js
+++ src/components/ts-subtitle/ts-subtitle.stories.js
@@ -4,6 +4,50 @@ import './index.js';
 export default {
     title: 'Components/TsSubtitle',
     component: 'ts-subtitle',
+    parameters: {
+        docs: {
+            description: {
+                component: 'The <code><ts-subtitle></code> component is used to display a subtitle.',
+            },
+            source: {
+                code: `
+
+import '@transdevoficial/ts-ds-core/dist/components/ts-subtitle';
+
+<ts-subtitle
+    label='string'
+    inverse='boolean'>
+</ts-subtitle>
+                `,
+            }
+        }
+    },
+    argTypes: {
+        label: {
+            name: 'label',
+            description: 'Label of the subtitle',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'Label' },
+                category: 'Inputs',
+            },
+            control: {
+                type: 'text',
+            },
+        },
+        inverse: {
+            name: 'inverse',
+            description: 'Inverse style',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'false' },
+                category: 'State',
+            },
+            control: {
+                type: 'boolean',
+            },
+        },
+    },
 }
 
 export const TsSubtitle = (args) => html`
