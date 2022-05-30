4dd90091ccb1397cd7c06e4b3257041ae9e0916c
elo9001535 <diana.pontes@elo.com.br>
2022-05-30 10:19:18 -0300

    export heading

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./components/ts-button";
 import "./components/ts-button-group";
 import "./components/ts-button-icon";
 import "./components/ts-check-box";
+import "./componentes/ts-heading";
 import "./components/ts-icon";
 import "./components/ts-link";
 import "./components/ts-paragraph";
