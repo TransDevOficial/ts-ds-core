ed2c062018e968676734841e26081d8543f7be72
elo9001535 <diana.pontes@elo.com.br>
2022-05-29 21:46:11 -0300

    fix checkbox exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./components/ts-button";
 import "./components/ts-button-group";
 import "./components/ts-button-icon";
-import "./components/ts-checkbox";
+import "./components/ts-check-box";
 import "./components/ts-icon";
 import "./components/ts-link";
 import "./components/ts-paragraph";
